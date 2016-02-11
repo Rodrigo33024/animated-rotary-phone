549c4fdb43a20af610ec0781e38d2767e8bcbce1
Rodrigo33024 <Rodrigo33024@gmail.com>
2016-02-11 14:01:12 -0500

    Adds sign up functionality with tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email)}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
 end
