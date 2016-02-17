0a94d56cdf961b3a2859118405f37d75df8e3cf0
Rodrigo33024 <Rodrigo33024@gmail.com>
2016-02-17 10:44:18 -0500

    Adds Edit user functions and test

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:rodrigo)
+  end
+
+  test "invalid edit" do
+    get edit_user_path(@user)
+
+    patch user_path(@user), user: {name: "", email: "testuserint@invalid", password: "123456", password_confirmation: "123456"}
+
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+
+    patch user_path(@user), user: {name: "Rodrigo R", email: "testuserint@valid.com", password: "", password_confirmation: ""}
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, "Rodrigo R"
+    assert_equal @user.email, "testuserint@valid.com"
+
+  end
+
+end
