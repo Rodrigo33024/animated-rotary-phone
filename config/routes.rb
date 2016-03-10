013d1d9d30607a4e3f7e8c0679ece0a30a51387b
Rodrigo33024 <Rodrigo33024@gmail.com>
2016-03-10 09:26:05 -0500

    Password reset WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'account_activations/edit'
 
   get 'signup'    => 'users#new'
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
